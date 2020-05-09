731ea167fce6402c739e78141d1081018aa63bef
Karan Nehra <karanehra@gmail.com>
2020-05-10 03:50:28 +0530

    Working with image data

--- main.js
+++ main.js
@@ -20,5 +20,23 @@ function getRandomInt() {
 let cvs = document.getElementById("cvs");
 let ctx = cvs.getContext("2d");
 let { width, height } = cvs.getBoundingClientRect();
+console.log(width, height);
 
 let size = width * height;
+ctx.imageSmoothingEnabled = false;
+let imageData = ctx.createImageData(width, height);
+let mem = new Uint32Array(imageData.data.buffer);
+console.log(mem);
+let i = 0,
+  j = 0;
+(function render() {
+  requestAnimationFrame(render);
+  mem.set([0xff00ffff], i + j * width);
+  console.log(i, width);
+  i++;
+  if (i > width / 3) {
+    i = 0;
+    j++;
+  }
+  ctx.putImageData(imageData, 0, 0); // apply image buffer
+})();
