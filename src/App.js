eb49f5b5d9529dd02764ade6cc4abcfcbbbe2667
Karan Nehra <karanehra@gmail.com>
2022-05-19 00:43:28 +0530

    Changed to tailwind

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-blue-600 w-screen h-screen flex flex-col items-center justify-center">
+      <div className="text-6xl font-black text-white uppercase tracking-widest">
+        Karan Nehra
+      </div>
+    </div>
+  );
+};
+
+export default App;
